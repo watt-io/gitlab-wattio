921d631864fc3d205f0e2f065db45829c41a0f7c
henrique <holiveira@wattio.com.br>
2023-10-02 07:41:41 -0300

    updated backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,22 @@
-cd /etc/gitlab
+#!/bin/bash
 
+# Define the variable with a default value
+BUCKET_NAME="gitlab-wattio"
+
+# Check if the first command-line argument is provided
+if [ $# -eq 1 ]; then
+    BUCKET_NAME="$1"
+fi
+
+# Change directory to /etc/gitlab
+cd /etc/gitlab_ce_scripts
+
+# Restart GitLab
 gitlab-ctl restart
+
+# Create a GitLab backup
 gitlab-backup create BACKUP=dump
-aws s3 cp gitlab.rb s3://gitlab-wattio/gitlab.rb
-aws s3 cp gitlab-secrets.json s3://gitlab-wattio/gitlab-secrets.json
+
+# Copy configuration files to S3 with the variable
+aws s3 cp gitlab.rb s3://$BUCKET_NAME/gitlab.rb
+aws s3 cp gitlab-secrets.json s3://$BUCKET_NAME/gitlab-secrets.json
